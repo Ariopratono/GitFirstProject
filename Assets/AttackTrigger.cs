dba658cb8975c43f6df613f4694b515718c34ea1
Ariopratono <70307702+Ariopratono@users.noreply.github.com>
2024-02-21 12:23:12 +0700

    First Commit
    Update 2024.2.21.1
    - Add Animation Idle & Attack
    - Trigger Animation Attack
    - Add 2D Asset to Scene

---
+++ Assets/AttackTrigger.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackTrigger : MonoBehaviour
+{
+    public Animator charAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Bisa Jalan");
+        charAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+
+    void AttackNow()
+    {
+        charAnimator.SetTrigger("GetAttack");
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            print("Spaci Kepencet");
+            AttackNow();
+        }
+    }
+}
